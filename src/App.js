23c321ee5b5371457ded3280a8b71cf43ca9af80
FeiteLu <79525180+FeiteLu@users.noreply.github.com>
2023-03-26 22:10:10 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,54 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
+import React from "react";
+import "./App.css";
+import TodoList from "./components/TodoList/TodoList";
+import Layout from "./components/Layout/Layout";
+import Dashboard from "./components/Dashboard/Dashboard";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      todo_list: []
+  state = {
+    activePage: "Dashboard",
+  };
+
+  handleChangeActivePage = (newActivePage) => {
+    console.log(newActivePage);
+    this.setState({
+      activePage: newActivePage,
+    });
+  };
+
+  render() {
+    let content = null;
+    switch (this.state.activePage) {
+      case "Dashboard":
+        content = <Dashboard />;
+        break;
+      case "TodoList":
+        content = <TodoList />;
+        break;
+      default:
+        break;
     }
+
+    return (
+      <Layout handleChangeActivePage={this.handleChangeActivePage}>
+        {content}
+      </Layout>
+    );
   }
-
-  async addTodo(newTodo) {
-    if (newTodo.trim() === "") {
-      alert("Please input title!");
-      return;
-    }
-    const newTd = { newTodo };
-
-    fetch(URL, {
-      method: "POST",
-      body: JSON.stringify(newTodo),
-      headers: { "Content-Type": "application/json" },
-    }).then((res) => res.json())
-      .then((res) => {
-        this.setState({ todo_list: [res, ...this.state.todo_list] });
-      })
-      .catch((err) => {
-        alert(`add new task failed: ${err}`);
-      });
-
-  }
-
-  async removeTodo(id) {
-    fetch(URL + `/${id}`, {
-      method: "DELETE",
-    }).then((res) => res.json())
-      .then(res => {
-        const new_list = this.state.todo_list.filter(todo => +todo.id !== +id);
-        this.setState({ todo_list: new_list });
-      })
-  }
-
-  async getTodos() {
-    fetch(URL).then((res) => res.json())
-  }
-
 }
-
-
-
-
 
 export default App;
